5f6c4c7daa33cebfaa78d3970c5c8f24b568d81b
WeRockStar <kotchaphan.m@gmail.com>
2018-06-16 13:56:22 +0700

    Add some of example

--- app/src/main/java/com/werockstar/materialcomponent/MainActivity.kt
+++ app/src/main/java/com/werockstar/materialcomponent/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.werockstar.materialcomponent
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
